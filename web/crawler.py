7f6c4373c18e27120c7ef4e4973382455e8a1133
Tasuku Bobcorn <devbobcorn@outlook.com>
2022-12-01 22:22:05 +0800

    add interruption handling and randomize request delay

--- web/crawler.py
+++ web/crawler.py
@@ -1,54 +1,89 @@
-import requests, base64, bs4, os, ssl
+import requests, base64, bs4, os, json, time, random
 from bs4 import BeautifulSoup
 
+# Ignore ssl hostname matching check (uncomment to enable if necessary)
+#import ssl
+#ssl.match_hostname = lambda cert, hostname: True
+
 # First get the full URL
-domain = "xi" + base64.b64decode("VXJl").decode("utf-8") + "nb."
+domain = f"xi{base64.b64decode('VXJl').decode('utf-8')}nb."
 folder = base64.b64decode("L1hpdVJlbi8=").decode("utf-8")
-root = "https://www." + domain + "vip"
-picroot = "https://p." + domain + "top"
+root = fr"https://www.{domain}vip"
+picroot = fr"https://p.{domain}top"
+
+proxies = {
+    'http': r'http://localhost:19180',
+    'https': r'http://localhost:19180'
+}
 
 curidx = 0
 lnx = {}
+
+# Current image count
 imgcnt = 0
+
+# Image count until last page
+lastimgcnt = 0
 
 def getImage(url):
     global curidx, imgcnt
 
-    #ssl.match_hostname = lambda cert, hostname: True
-    resp = requests.get(url, stream=True)
+    resp = requests.get(url, stream=True, proxies=proxies)
     resp.encoding = resp.apparent_encoding
 
-    if resp.status_code == 200:
-        # Ready
-        imgcnt = imgcnt + 1
-        with open(r'downloaded/' + str(curidx) + '/' + str(imgcnt) + '.jpg', 'wb') as f:
+    if resp.status_code == 200: # Ready
+        with open(fr'downloaded/{curidx}/{imgcnt}.jpg', 'wb') as f:
             f.write(resp.content)
     else:
-        print("Failed to download image from " + url)
+        raise Exception(f"Failed to grab image from {url}. Error Code: {resp.status_code}")
 
 def getCollection(idx):
     global lnx, imgcnt, curidx
     curidx = idx  # Set current processing collection index
     lnx.clear()   # Reset links dictionary
     imgcnt = 0    # Reset image count
-    # Create the directory for storing images
-    if not os.path.exists("downloaded/" + str(idx)):
-        os.makedirs("downloaded/" + str(idx))
-    print('Collection ' + idx + ' starts...')
-    # First search the index page, and get links to the rest of current collection
-    getPage(folder + str(idx) + ".html")
+    lastimgcnt = 0
+
+    print(f'Collection #{idx} starts...')
+    
+    if not os.path.exists(fr"downloaded/{idx}"): # Create the directory for storing images
+        os.makedirs(fr"downloaded/{idx}")
+        # First search the index page, and get links to the rest of current collection
+        getPage(f"{folder}{idx}.html")
+    else: # Look for download_info.json if the folder presents
+        if os.path.exists(fr"downloaded/{idx}/download_info.json"):
+            with open(fr'downloaded/{idx}/download_info.json', 'r+') as f:
+                print(f'Restoring download process for collection #{idx}.')
+                infotxt = f.read()
+                jsonObj = json.loads(infotxt)
+
+                for lnk, vis in jsonObj['links'].items():
+                    lnx[lnk] = vis # Restore this link
+                
+                lastimgcnt = imgcnt = int(jsonObj['image_count_till_last_page'])
+                print(f'lastimgcnt restored to {lastimgcnt}')
+
+            if f"{folder}{idx}.html" in lnx.keys() and lnx[f"{folder}{idx}.html"]:
+                print("Landing page already collected, skip...")
+            else:
+                getPage(f"{folder}{idx}.html")
+                
+        else:
+            # First search the index page, and get links to the rest of current collection
+            getPage(f"{folder}{idx}.html")
+    
     for lnk, vis in lnx.items():
         if not vis:
             getPage(lnk)
-    print("Collection complete: " + str(len(lnx)) + " pages searched, " + str(imgcnt) + " images found.")
+    print(f"Collection complete: {len(lnx)} pages searched. {imgcnt} images found.\n")
 
 def getPage(path):
-    global lnx
-    if path not in lnx or not lnx[path]: # Short-circuit logic here
-        lnx[path] = True
+    global lnx, imgcnt, lastimgcnt
 
-    #ssl.match_hostname = lambda cert, hostname: True
-    resp = requests.get(root + path)
+    # Update last image count
+    lastimgcnt = imgcnt;
+
+    resp = requests.get(f"{root}{path}", proxies=proxies)
     resp.encoding = resp.apparent_encoding
 
     if resp.status_code == 200:
@@ -71,18 +106,27 @@ def getPage(path):
                     if navCont != None and navc == None:
                         navc = navCont.find(name='div', attrs={'class':'page'})
 
-        if cont != None:
-            print("Images found on " + path + ":")
+        if cont != None: # Image container is successfully found
+            print(f"Images found on {path}:")
             # Process Images...
             for img in cont.findAll(name='img'):
-                # For each image container
-                print(img.attrs['src'])
-                getImage(root + img.attrs['src'])
-            # Sleep for a short period of time so that
-            # We ain't gonna be blocked
-            time.sleep(4)
+                # For each image element
+                imgcnt += 1
+                print(f"Grabbing [{curidx}:{imgcnt}] {img.attrs['src']}", end='')
+                # Sleep for a short period of time so that we ain't gonna be blocked
+                time.sleep(0.2 + random.random())
+                print('...')
+                getImage(f"{root}{img.attrs['src']}")
+            
+            # Sleep for a short period of time so that we ain't gonna be blocked
+            time.sleep(0.5 + random.random())
 
-        if navc != None:
+        # All images collected, mark as visited(or add into the dictionary if
+        # it is not yet present)
+        if path not in lnx or not lnx[path]: # Short-circuit logic here
+            lnx[path] = True
+
+        if navc != None: # Navigation bar container is successfully found
             # Process Navigation Links...
             for lnk in navc.findAll(name='a'):
                 if 'href' in lnk.attrs.keys() and lnk.attrs['href'] not in lnx:
@@ -91,19 +135,44 @@ def getPage(path):
         else:
             print("Main content cannot be found!")
     else:
-        print("Error Code: " + str(resp.status_code))
+        raise Exception(f"Error Code: {resp.status_code}")
+        
 
 with open(r'list.txt') as f:
     lnz = f.readlines()
-    cnt = 0
+    collecount = 0
+
+    commented = False
+
     for ln in lnz:
         ln = ln.strip()
-        if ln == '':
+
+        if (ln.upper().startswith('#E')): # Comment section end
+            commented = False
+            continue
+        elif ln.upper().startswith('#S'): # Comment section start
+            commented = True
+            continue
+
+        if commented or ln == '' or ln.startswith('#'):
             continue
         
-        cnt = cnt + 1
+        collecount = collecount + 1
         idxs = ln.strip().split(' ', 2)
         idx = idxs[1].replace('+','').replace('*','')
-        getCollection(idx)
-        if cnt >= 5:
+
+        try:
+            getCollection(idx)
+        except Exception as e:
+            # Dump current info
+            jsonObj = json.dumps({
+                'current_index': curidx,
+                'links': lnx,
+                'image_count_till_last_page': lastimgcnt
+            })
+            with open(fr'downloaded/{idx}/download_info.json', 'w+') as f:
+                f.write(jsonObj)
+
+            print(f"Un4tun8ly, the download process is interrupted: {e}")
+            print(fr"Download info of this collection can be found here: downloaded/{curidx}/download_info.json")
             break
