a02874736b91b7d5ea622ce2581a03333b177015
Tasuku Bobcorn <DevBobcorn@outlook.com>
2023-01-08 18:23:52 +0800

    :fire: Delete scripts no longer useful

--- web/crawler.py
+++ web/crawler.py
@@ -157,42 +157,91 @@ def getPage(path):
 
 os.chdir('../')
 
-idx = 5650
+# Cover Crwaler ===================================================================================
+'''
+idx = 1
 
-while idx < 10001:
+while idx < 20001:
     try:
         getCoverImage(idx)
         time.sleep(0.4 + random.random())
         idx += 1
     except Exception as e:
         print(f'Exception occurred while getting cover for #{idx}: {e} Retrying...')
+'''
 
 
-with open(r'web\list.txt') as f:
-    lnz = f.readlines()
-    collecount = 0
+# Content Crawler =================================================================================
+cats = [
+    'XiuRen',   'MFStar',   'MiStar',   'MyGirl',
+    'Imiss',    'BoLoli',   'YouWu',    'Uxing',
+    'MiiTao',   'FeiLin',   'WingS',    'Taste',
+    'LeYuan',   'HuaYan',   'DKGirl',   'MintYe',
+    'YouMi',    'Candy',    'MTMeng',   'Micat',
+    'HuaYang',  'XingYan',  'XiaoYu'
+]
 
-    commented = False
+col2cat  = [ ]
+col2desc = [ ]
+col2page = [ ]
 
-    for ln in lnz:
-        ln = ln.strip()
+listLen = 13000
 
-        if (ln.upper().startswith('#E')): # Comment section end
-            commented = False
-            continue
-        elif ln.upper().startswith('#S'): # Comment section start
-            commented = True
-            continue
+latestCoverIdx = 0
 
-        if commented or ln == '' or ln.startswith('#'):
-            continue
-        
-        collecount = collecount + 1
-        idxs = ln.strip().split(' ', 2)
-        idx = idxs[1].replace('+','').replace('*','')
+for i in range(listLen):
+    col2cat.append(-1)
+    col2desc.append('')
+    col2page.append('')
 
+# Load all the category mappings
+for catIdx in range(len(cats)):
+    cat = cats[catIdx]
+
+    leadingCatNameLower = f'{cat.lower()}'
+
+    with open(f'web/cats/cat_dict_{cat.lower()}.json') as f:
+        catText = f.read()
+        catDict = json.loads(catText)
+
+        for coverImgIdx in catDict: # For each collection
+            # Remove duplicate leading category names in page url, and getting only
+            # the collection index number  (only to deal with a few exceptions)
+            colIdx = int(coverImgIdx)
+
+            if colIdx > latestCoverIdx:
+                latestCoverIdx = colIdx
+    
+            if colIdx > 0 and colIdx < listLen:
+                if col2cat[colIdx] != -1:
+                    print(f'Collection #{colIdx} previously registered in another category! Overwriting! ({cats[col2cat[colIdx]]} => {cats[catIdx]})')
+
+                col2cat[colIdx]  = catIdx
+                col2desc[colIdx] = catDict[coverImgIdx]['desc']
+                col2page[colIdx] = catDict[coverImgIdx]['pageIdx']
+            else:
+                print(f'Collection index out of bound: {colIdx}')
+
+ci = 1
+
+while ci < 20001:
         try:
-            getCollection(idx)
+            catIdx = col2cat[ci]
+
+            if catIdx != -1:
+                pageLink = f'[{cats[catIdx]}/{col2page[ci]}]'
+
+                print(f'{str(ci).rjust(5)} {pageLink.ljust(20)} {col2desc[ci].ljust(20)}')
+
+                # Get this collection
+                getCollection(ci)
+
+                # Clone the cover file
+                #shutil.copyfile(f'web/downloaded/covers/{ci}.jpg', f'web/downloaded/selected/{ci}.jpg')
+            else:
+                print(f'Category for collection #{str(ci)} is unknown')
+                
+                
         except Exception as e:
             print(f"Un4tun8ly, the download process is interrupted: {e}")
 
@@ -202,7 +251,7 @@ with open(r'web\list.txt') as f:
                 'links': lnx,
                 'image_count_till_last_page': lastimgcnt
             })
-            with open(f'{dlPath}/{idx}/download_info.json', 'w+') as f:
+            with open(f'{dlPath}/{ci}/download_info.json', 'w+') as f:
                 f.write(jsonObj)
             
             print(f"Download info of this collection can be found here: downloaded/{curColPath}/download_info.json")
